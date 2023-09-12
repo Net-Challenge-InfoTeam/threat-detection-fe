3ba38f6093b6c3028b3665db405a518d23b18d3a
Dohyun-Ko <dohyun682@gmail.com>
2023-09-13 00:54:22 +0900

    :sparkles: Change api endpont

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -13,6 +13,31 @@ import styled from "styled-components";
 
 interface HomePageProps {}
 
+const data = [
+  {
+    id: 1,
+    location: {
+      longitude: 126.84096217263814,
+      latitude: 35.228096665528106,
+    },
+    kind: "칼",
+    count: 3,
+    acc: [0.5, 0.6, 0.7],
+    detectedAt: new Date("2023-09-01T14:58:58.000Z"),
+  },
+  {
+    id: 1,
+    location: {
+      longitude: 126.84157217263814,
+      latitude: 35.2283966655281,
+    },
+    kind: "담배",
+    count: 3,
+    acc: [0.5, 0.6, 0.7],
+    detectedAt: new Date("2023-09-01T14:58:58.000Z"),
+  },
+];
+
 const HomePage = ({}: HomePageProps) => {
   const [safety, setSafety] = useState<Safety>(Safety.SAFE);
   const mapViewerRef = useRef<HTMLDivElement>(null);
--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     proxy: {
       //CORS 방지를 위한 프록시 설정입니다.
       "/local": {
-        target: "http://10.32.103.147:3000", //실제 api 도메인을 넣어줍니다.
+        target: "https://61.252.59.44", //실제 api 도메인을 넣어줍니다.
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/local/, ""), //target url을 교체합니다.
       },
